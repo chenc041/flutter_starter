4a6a8a50fb3bc667b0d627effd52fdafe1b67442
chenc <double_cl@163.com>
2024-11-17 20:52:21 +0800

    feat: add photo_manager(https://github.com/fluttercandies/flutter_photo_manager) for get device files

--- lib/home/index.dart
+++ lib/home/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +9,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Future<void> getFiles() async {
+    await PhotoManager.requestPermissionExtend();
+    final List<AssetPathEntity> paths = await PhotoManager.getAssetPathList(
+      type: RequestType.audio,
+    );
+    for (AssetPathEntity path in paths) {
+      if (path.name == 'CallRecord') {
+        debugPrint("item ===>${path.toString()}");
+        final List<AssetEntity> entities =
+            await path.getAssetListPaged(page: 0, size: 80);
+        debugPrint('entities ===>${entities.toString()}');
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getFiles();
+  }
 
   @override
   Widget build(BuildContext context) {
